bd0476407253194b0b20cedb71207a3691f160be
Moanrisy <moanrisy@gmail.com>
2024-01-15 14:30:10 +0700

    v0.3 input without press Enter key

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.6
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli v1.22.14 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,24 +7,28 @@ import (
 	"os/exec"
 	"sort"
 
+	"github.com/eiannone/keyboard"
 	"github.com/urfave/cli"
 )
 
-func sortMapKeys(m map[string]string) []string {
-	keys := make([]string, 0, len(m))
+func sortMapKeys(m map[rune]string) []rune {
+	keys := make([]rune, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
 	}
-	sort.Strings(keys)
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
 	return keys
 }
 
 func main() {
-	links := map[string]string{
-		"t": "https://tailwindcss.com/docs/installation",
-		"g": "https://gobyexample.com/",
-		"l": "https://www.lazyvim.org/keymaps",
-		"a": "https://aur.archlinux.org/",
+	links := map[rune]string{
+		't': "https://tailwindcss.com/docs/installation",
+		'g': "https://gobyexample.com/",
+		'l': "https://www.lazyvim.org/keymaps",
+		'a': "https://aur.archlinux.org/",
+		'm': "https://www.myanonamouse.net/tor/browse.php?&tor%5BsrchIn%5D%5Btitle%5D=true&tor%5BsrchIn%5D%5Bauthor%5D=true&tor%5BsearchType%5D=all&tor%5BsearchIn%5D=torrents&tor%5Bcat%5D%5B%5D=53&tor%5Bcat%5D%5B%5D=75&tor%5Bcat%5D%5B%5D=0&tor%5BbrowseFlagsHideVsShow%5D=0&&&tor%5BminSize%5D=0&tor%5BmaxSize%5D=0&tor%5Bunit%5D=1&tor%5BminSeeders%5D=0&tor%5BmaxSeeders%5D=0&tor%5BminLeechers%5D=0&tor%5BmaxLeechers%5D=0&tor%5BminSnatched%5D=0&tor%5BmaxSnatched%5D=0&&tor%5BsortType%5D=default&tor%5BstartNumber%5D=0&thumbnail=true",
 	}
 
 	app := &cli.App{
@@ -33,14 +37,16 @@ func main() {
 		Action: func(*cli.Context) error {
 			sortedKeys := sortMapKeys(links)
 			for _, k := range sortedKeys {
-				fmt.Println(k, links[k])
+				fmt.Println(string(k), links[k])
 			}
 
-			fmt.Print("Type a number: ")
-			var i string
-			fmt.Scanln(&i)
+			char, _, err := keyboard.GetSingleKey()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("You pressed: %q\r\n", char)
 
-			exec.Command("firefox", links[i]).Run()
+			exec.Command("firefox", links[char]).Run()
 			return nil
 		},
 	}
